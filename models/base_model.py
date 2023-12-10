e4e97902b40f15ef178191e675a97168d99a8c4e
qtilicous <qtilicousbbz@gmail.com>
2023-12-10 12:31:10 -0800

    Dont know what going on just adding

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,60 @@
+#!/usr/bin/python3
+"""
+Module documentation here
+"""
 import uuid
 from datetime import datetime
 
 class BaseModel:
+    """
+    Class documentation here
+    """
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        """
+        Initializes a new instance of the BaseModel class.
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
+                elif key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        Returns the string representation of the BaseModel object.
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        # Save the instance to a JSON file (implementation needed)
+        """
+        Updates the public instance attribute updated_at with the current datetime.
+        """
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        # Convert instance attributes to a dictionary (implementation needed)
+        """
+        Returns a dictionary containing all keys/values of __dict__ of the instance.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
+# If the script is run directly, execute the sample test
+if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
