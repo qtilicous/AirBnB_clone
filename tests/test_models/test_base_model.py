ea17ae4b66ec5e9599468b2d7ecfa271407cf4cd
qtilicous <qtilicousbbz@gmail.com>
2023-12-11 23:32:32 -0800

    Fix file test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,47 +1,75 @@
-import unittest
-from models.base_model import BaseModel
+#!/usr/bin/python3
+"""
+This module defines the HBNBCommand class.
+A command-line interpreter for the AirBnB clone.
+"""
+import cmd
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
-class TestBaseModel(unittest.TestCase):
-    def test_instance_creation(self):
-        # Test creation of a BaseModel instance
-        obj = BaseModel()
-        self.assertIsInstance(obj, BaseModel)
+class HBNBCommand(cmd.Cmd):
+    """
+    HBNBCommand class is a command-line interpreter for the AirBnB clone.
+    """
+    prompt = '(hbnb) '
 
-    def test_attribute_assignment(self):
-        # Test attribute assignment
-        obj = BaseModel()
-        obj.name = "Test Object"
-        self.assertEqual(obj.name, "Test Object")
+    def do_create(self, arg):
+        """
+        Create a new instance of a specified class, save it, and print its id.
 
-        def test_to_dict_method(self):
-            # Test the to_dict method
-            obj = BaseModel()
-            obj.name = "Test Object"
-            obj_dict = obj.to_dict()
-
-            self.assertIn('id', obj_dict)
-            self.assertIn('created_at', obj_dict)
-            self.assertIn('updated_at', obj_dict)
-            self.assertIn('name', obj_dict)
-
-            self.assertEqual(obj_dict['id'], obj.id)
-            self.assertEqual(obj_dict['created_at'], obj.created_at.isoformat())
-            self.assertEqual(obj_dict['updated_at'], obj.updated_at.isoformat())
-            self.assertEqual(obj_dict['name'], 'Test Object')
-
-
-        expected_dict = {
-            'id': obj.id,
-            'created_at': obj.created_at.isoformat(),
-            'updated_at': obj.updated_at.isoformat(),
-            'name': 'Test Object'
+        Usage: create <class_name>
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        classes = {
+            'BaseModel': BaseModel,
+            'State': State,
+            'City': City,
+            'Amenity': Amenity,
+            'Place': Place,
+            'Review': Review,
+            'User': User
         }
+        if arg not in classes:
+            print("** class doesn't exist **")
+            return
+        new_instance = classes[arg]()
+        new_instance.save()
+        print(new_instance.id)
 
-        self.assertEqual(obj_dict, expected_dict)
+    def do_show(self, arg):
+        """
+        Print the string representation of an instance
+        based on the class name and id.
 
-    # Add more tests as needed for BaseModel and other classes
+        Usage: show <class_name> <id>
+        """
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        if args[0] not in classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        key = args[0] + '.' + args[1]
+        all_objs = storage.all()
+        if key in all_objs:
+            print(all_objs[key])
+        else:
+            print("** no instance found **")
+
+    # Add similar methods for 'destroy', 'all', and 'update'
 
 
 if __name__ == '__main__':
-    unittest.main()
+    HBNBCommand().cmdloop()
